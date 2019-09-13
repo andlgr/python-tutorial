83d3d6dcb977d9d66082e887087fc6b81dcbeb61
Andre Reis <andre.lgr@gmail.com>
2019-09-13 13:37:05 -0300

    Incrementing reference example

--- src/references.py
+++ src/references.py
@@ -1,21 +1,27 @@
 # Learning Python 101
 
-class Reference:
+class ClassWithReferenceToDependency:
     def __init__(self, param):
-        self.__param_ = param
+        if not isinstance(param, list):
+            raise Exception(
+                "Please pass object inside a list in order to keep reference inside ClassWithReferenceToDependency")
+        self.__param_ = param  # A mutable type like list behaves like a shared_ptr in C++. To copy, use param.copy()
         print("Is the same? " + str(param is self.__param_))
 
     def change_param(self):
-        print("On function - received: " + str(self.__param_[0]))
+        print("change_param - current: " + str(self.__param_[0]))
         self.__param_[0] = 10
-        print("On function - changed: " + str(self.__param_[0]))
+        print("change_param - changed: " + str(self.__param_[0]))
+
 
 def main():
     param = [800]
     print("On main - before call: " + str(param[0]))
-    obj = Reference(param)
+    obj = ClassWithReferenceToDependency(param)
     obj.change_param()
     print("On main - after call: " + str(param[0]))
+    assert(10 == param[0])
+
 
 if __name__ == "__main__":
     main()
