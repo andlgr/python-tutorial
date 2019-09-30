9eac753f319e148ba9833e5d9d39c82809543e01
Andre Reis <andre.lgr@gmail.com>
2019-09-30 16:53:48 -0300

    Incrementing C++ and Python integration example

--- src/cpp-module/module.cpp
+++ src/cpp-module/module.cpp
@@ -3,15 +3,41 @@
 
 using namespace boost::python;
 
+class MyPrinter {
+ public:
+    MyPrinter() : value_(100) {}
+
+    MyPrinter(const MyPrinter& other) : value_(other.value_) {}
+
+    void Print() {
+        fprintf(stdout, "I am inside a C++ module (dependency) [value = %d]!\n", value_);
+        value_++;
+    }
+ private:
+    int value_;
+};
+
 class MyModule {
  public:
     int PrintFromCppModule(int value) {
         fprintf(stdout, "I am inside a C++ module [value = %d]!\n", value);
         return 0;
     }
+
+    void PrintFromOtherObject(MyPrinter* printer) {
+        if (nullptr == printer) {
+            fprintf(stderr, "Invalid pointer\n");
+            return;
+        }
+        printer->Print();
+    }
 };
 
 BOOST_PYTHON_MODULE(module) {
-    class_<MyModule>("MyModule")
-        .def("PrintFromCppModule", &MyModule::PrintFromCppModule);
+    class_<MyPrinter>("MyPrinter", init<>())
+        .def("Print", &MyPrinter::Print);
+
+    class_<MyModule>("MyModule", init<>())
+        .def("PrintFromCppModule", &MyModule::PrintFromCppModule)
+        .def("PrintFromOtherObject", &MyModule::PrintFromOtherObject);
 }
--- src/cpp-module/module.py
+++ src/cpp-module/module.py
@@ -1,10 +1,31 @@
 # Learning Python 101
 
-import build.module
+import build.module  # Our C++ shared library is located at ./build/module.so
 
 def main():
+    printer = build.module.MyPrinter()
     cpp_obj = build.module.MyModule()
+
     cpp_obj.PrintFromCppModule(80)
+    cpp_obj.PrintFromOtherObject(printer)  # 'printer' is being passed as pointer
+
+    print("Now printing from python:")
+    printer.Print()
+    cpp_obj.PrintFromOtherObject(printer)
+    cpp_obj.PrintFromOtherObject(None)  # Equivalent to nullptr
+    printer.Print()
+    printer.Print()
+
+    printer_2 = printer  # Doesn't copy - equivalent to reference
+    cpp_obj.PrintFromOtherObject(printer)
+    cpp_obj.PrintFromOtherObject(printer)
+    cpp_obj.PrintFromOtherObject(printer)
+    cpp_obj.PrintFromOtherObject(printer_2)
+
+    printer = None  # Sets to nullptr
+    cpp_obj.PrintFromOtherObject(printer)
+    cpp_obj.PrintFromOtherObject(printer_2)
+
 
 if __name__ == "__main__":
     main()
